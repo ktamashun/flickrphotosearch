e13cb0c5e740a2983630607608a9650aa0646631
ktamashun <kovacs.tamas.hun@gmail.com>
2017-12-11 00:49:03 +0100

    Api-gateway code cleanup

--- backend/main.go
+++ backend/main.go
@@ -2,30 +2,18 @@ package main
 
 import (
 	"log"
-	"net/http"
-
-	"github.com/ktamashun/flickrphotosearch/backend/app"
 	"github.com/ktamashun/flickrphotosearch/backend/apis"
 	"github.com/ktamashun/flickrphotosearch/backend/app/servers"
 	"net"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc"
-	"fmt"
 )
 
 const (
-	restPort = ":8080"
 	grpcPort = ":18000"
 )
 
 func main() {
-	// JSONAPI
-	go func() {
-		router := app.CreateRouter()
-		log.Fatal(http.ListenAndServe(restPort, router))
-	}()
-
-	fmt.Println("hello grpc world")
 	// gRPC API
 	lis, err := net.Listen("tcp", grpcPort)
 	if err != nil {
